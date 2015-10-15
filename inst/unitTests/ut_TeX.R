cfbe000074316d4b96d935bbcd317dc858855287
Enrico Schumann <es@enricoschumann.net>
2015-10-15 06:51:25 +0200

    disabled tests for TeXunits

--- inst/unitTests/ut_TeX.R
+++ inst/unitTests/ut_TeX.R
@@ -1,16 +1,16 @@
 ## -*- truncate-lines: t; -*-
-## Time-stamp: <2015-05-07 17:03:58 CEST (es)>
+## Time-stamp: <2015-10-15 06:51:03 CEST (es)>
 
 test.TeXunits <- function() {
 
     ## TeXunits(c("1 cm", "0.7 in"), "in")
     ## TeXunits("1 cm", c("in", "cm"))
 
-    require("textutils")
-    require("RUnit")
+    ## require("textutils")
+    ## require("RUnit")
 
-    tmp <- TeXunits(c("1 cm", "0.7 in"), c("in", "cm"))
-    checkEqualsNumeric(as.numeric(tmp), 
-                       c(0.393700804026445, 1.77799992492009))
-    checkEquals(names(tmp) , c("in", "cm"))
+    ## tmp <- TeXunits(c("1 cm", "0.7 in"), c("in", "cm"))
+    ## checkEqualsNumeric(as.numeric(tmp), 
+    ##                    c(0.393700804026445, 1.77799992492009))
+    ## checkEquals(names(tmp) , c("in", "cm"))
 }
