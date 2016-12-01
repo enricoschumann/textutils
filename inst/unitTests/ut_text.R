f542061a873bcf574038d3654bbb8278c96344a2
Enrico Schumann <es@enricoschumann.net>
2016-12-01 10:09:35 +0100

    Move unit tests to single file

--- inst/unitTests/ut_text.R
+++ inst/unitTests/ut_text.R
@@ -20,3 +20,17 @@ test.spaces <- function() {
                 c("", " ", "  ", "   "))
 
 }
+
+test.TeXunits <- function() {
+
+    ## TeXunits(c("1 cm", "0.7 in"), "in")
+    ## tmp <- TeXunits(c("1 cm", "0.7 in"), c("in", "cm"))
+
+    require("textutils")
+    require("RUnit")
+
+    tmp <- TeXunits("1 cm", c("in", "cm"))
+    checkEqualsNumeric(as.numeric(tmp), 
+                       c(0.393700804026445, 1))
+    checkEquals(names(tmp) , c("in", "cm"))
+}
