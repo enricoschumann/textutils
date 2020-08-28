1f122924c3a37b65161429dacce7fe820034eb9b
Enrico Schumann <es@enricoschumann.net>
2020-08-28 10:21:46 +0200

    [toHTML] Make several updates for data.frame method
    fix handling of one-row data frames
    
    add new argument 'col.names', with default TRUE. If
    FALSE, column names of data frame are ignored
    
    'row.names' may now be either logical (as before) or a
    string, which is then used as column name

--- R/functions.R
+++ R/functions.R
@@ -32,9 +32,15 @@ toHTML.text <- function(x, ...){
 
 toHTML.data.frame <- function(x, ...,
                               row.names = FALSE,
+                              col.names = TRUE,
                               class.handlers = list(),
                               col.handlers = list()) {
 
+    row.names.header <- ""
+    if (is.character(row.names)) {
+        row.names.header <- row.names
+        row.names <- TRUE
+    }
     dfnames <- names(x)
     if (any(i <- names(col.handlers) %in% dfnames)) {
         elt <- which(i)
@@ -48,11 +54,14 @@ toHTML.data.frame <- function(x, ...,
         if (cl[j] %in% names(class.handlers))
             x[[j]] <- class.handlers[[ cl[j] ]](x[[j]])
     }
-    ans <- rbind(paste0("<th>", c(if (row.names) "", colnames(x)), "</th>"),
-                 cbind(if (row.names) paste0("<td>", row.names(x), "</td>"),
-                       apply(x, 2, function(x) paste0("<td>", x, "</td>"))))
+    header.row <- c(if (row.names) row.names.header, colnames(x))
+    m <- apply(x, 2, function(x) as.matrix(paste0("<td>", x, "</td>")))
+    if (dim(x)[[1]] == 1L)
+        dim(m) <- dim(x)
+    ans <- rbind(if (col.names) paste0("<th>", header.row, "</th>"),
+                 cbind(if (row.names) paste0("<td>", row.names(x), "</td>"), m))
 
-    paste("<tr>", apply(ans, 1, paste, collapse = ""), "</tr>")
+    paste0("<tr>", apply(ans, 1, paste, collapse = ""), "</tr>")
 }
 
 toLatex.data.frame <- function(object,
---
+++ inst/tinytest/test_toHTML.R
@@ -0,0 +1,59 @@
+## toHTML: data.frame
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6))
+expect_equal(s,
+             c("<tr><th>a</th><th>b</th></tr>",
+               "<tr><td>1</td><td>4</td></tr>",
+               "<tr><td>2</td><td>5</td></tr>",
+               "<tr><td>3</td><td>6</td></tr>"))
+
+
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6), row.names = TRUE)
+expect_equal(s,
+             c("<tr><th></th><th>a</th><th>b</th></tr>",
+               "<tr><td>1</td><td>1</td><td>4</td></tr>",
+               "<tr><td>2</td><td>2</td><td>5</td></tr>",
+               "<tr><td>3</td><td>3</td><td>6</td></tr>"))
+
+
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6), row.names = "Num")
+expect_equal(s,
+             c("<tr><th>Num</th><th>a</th><th>b</th></tr>",
+               "<tr><td>1</td><td>1</td><td>4</td></tr>",
+               "<tr><td>2</td><td>2</td><td>5</td></tr>",
+               "<tr><td>3</td><td>3</td><td>6</td></tr>"))
+
+
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6, row.names = letters[24:26]),
+            row.names = "Letter")
+expect_equal(s,
+             c("<tr><th>Letter</th><th>a</th><th>b</th></tr>",
+               "<tr><td>x</td><td>1</td><td>4</td></tr>",
+               "<tr><td>y</td><td>2</td><td>5</td></tr>",
+               "<tr><td>z</td><td>3</td><td>6</td></tr>"))
+
+
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6), col.names = FALSE)
+expect_equal(s,
+             c("<tr><td>1</td><td>4</td></tr>",
+               "<tr><td>2</td><td>5</td></tr>",
+               "<tr><td>3</td><td>6</td></tr>"))
+
+
+
+s <- toHTML(data.frame(a = 1:3, b = 4:6), col.names = FALSE, row.names = TRUE)
+expect_equal(s,
+             c("<tr><td>1</td><td>1</td><td>4</td></tr>",
+               "<tr><td>2</td><td>2</td><td>5</td></tr>",
+               "<tr><td>3</td><td>3</td><td>6</td></tr>"))
+
+
+## single row data.frame
+s <- toHTML(data.frame(a = 1, b = 4))
+expect_equal(s,
+             c("<tr><th>a</th><th>b</th></tr>",
+               "<tr><td>1</td><td>4</td></tr>"))
