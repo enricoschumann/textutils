cadbff934082ac4f0f7a9c93da8a135e91c2d43b
Enrico Schumann <es@enricoschumann.net>
2025-04-02 07:40:58 +0200

    Prepare version 0.4-2

--- inst/tinytest/test_HTMLrm.R
+++ inst/tinytest/test_HTMLrm.R
@@ -1,10 +1,10 @@
-test <- "before <a href='http://enricoschumann.net'>LINK</a>  after"
+test <- "before <a href='https://enricoschumann.net'>LINK</a>  after"
 
 expect_equal(HTMLrm(test),
-             "before http://enricoschumann.net  after")
+             "before https://enricoschumann.net  after")
 
 expect_equal(HTMLrm(paste0(test, test)),
-             paste(rep("before http://enricoschumann.net  after", 2),
+             paste(rep("before https://enricoschumann.net  after", 2),
                    collapse = ""))
 
 ## ---
@@ -15,9 +15,9 @@ expect_equal(HTMLrm("before <a>LINK</a> after"),
 
 ## ---
 
-test <- "before <a target = '_blank' href='http://enricoschumann.net'>LINK</a> after"
+test <- "before <a target = '_blank' href='https://enricoschumann.net'>LINK</a> after"
 expect_equal(HTMLrm(test),
-             "before http://enricoschumann.net after")
+             "before https://enricoschumann.net after")
 
 ## --- comments
 
--- inst/tinytest/test_insert.R
+++ inst/tinytest/test_insert.R
@@ -5,12 +5,15 @@ expect_equal(insert(1:5, 99, 3), c(1:2, 99, 3:5))
 expect_equal(insert(1:5, 98:99, c(1,5)), c(98, 1:4, 99, 5))
 expect_equal(insert(1:3, 101:103, 1:3), c(101,1,102,2,103,3))
 
-
+## recycling
 expect_equal(insert(letters[1:5], " ", c(2, 4)),
              c("a", " ", "b", "c", " ", "d", "e"))
 
 expect_equal(insert(letters[1:5], c(" ", "Z"), c(2, 4)),
              c("a", " ", "b", "c", "Z", "d", "e"))
+
+expect_equal(insert(letters[1:5], c(" ", "Z"), 2),
+             c("a", " ", "Z", "b", "c", "d", "e"))
 
 ## if "values" is of length 0, nothing is inserted
 expect_equal(insert(letters[1:5], character(0L), c(2, 4)),
